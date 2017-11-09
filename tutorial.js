38c89d59046c6b3ae93b2a242ccee8a35dddf1b9
girlswhocodeev <girlswhocodeev@gmail.com>
2017-11-09 12:16:10 -0800

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -1,13 +1,51 @@
+// comments
+// single line comment
+/* 
+multiline
+comment
+*/
+
 // variables
+// basic data types: number, string, boolean
+var x = 1; // declare and intialize
+x = 2; // re-assign
+var string = "hi";
+var bool = true;
 
 // conditionals
+if (5 > 6) {
+  // do something
+} else if (5 == 6) {
+  // do something else
+} else {
+  // do something else
+}
+
+// loops
+// for loop
+for (var x = 1; x < 3; x++) {
+  // do something
+}
+// while loop
+var foo = 1;
+while (foo < 5) {
+  foo++;
+  // do something
+}
 
 // functions
+function math() {
+  // do something
+}
 
 // buttons
+// (this is done in html, link to a function on js side)
 
 // alert
+alert("hello");
 
 // confirm
+confirm("hello");
 
 // prompt
+var input = prompt("enter a number");
